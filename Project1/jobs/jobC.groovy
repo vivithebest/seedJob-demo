d25c18ab4ee2072b92d0c6e2000fac10ce58b0cc
qinrui <ruqin@thoughtworks.com>
2020-11-23 15:36:35 +0800

    add a pipeline

---
+++ Project1/jobs/jobC.groovy
@@ -0,0 +1,25 @@
+pipelineJob('sssss-pipeline3') {
+  definition {
+    cps {
+      script('''
+        pipeline {
+            agent any
+                stages {
+                    stage('Stage 1') {
+                        steps {
+                            echo 'logic'
+                        }
+                    }
+                    stage('Stage 2') {
+                        steps {
+                            echo 'logic'
+                        }
+                    }
+                }
+            }
+        }
+      '''.stripIndent())
+      sandbox()     
+    }
+  }
+}
